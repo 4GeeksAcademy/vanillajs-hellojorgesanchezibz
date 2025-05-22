3928f9267974455d09d6a5300c68ca591cc901bb
Jorge Sanchez Ortiz <jorgesoibz@gmail.com>
2025-05-22 17:05:48 +0000

    Proyectocartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let valor = [ "A","2","3","4","5","6","7","8","9","J","Q","K"]
+  let suit = ["&spades;","&clubs;","&hearts;","&diams;"]
+  let valorRandom = valor[Math.floor(Math.random()*valor.length)];
+  let suitRandom = suit[Math.floor(Math.random()*suit.length)];
+
+  document.querySelector(".numero").innerHTML = valorRandom;
+  document.querySelector(".palo-alto").innerHTML = suitRandom;
+    document.querySelector(".palo-bajo").innerHTML = suitRandom;
+
+  
+  
 };
