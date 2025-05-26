3499e32c4609f79a733b829b93d023701adb9b85
Jorge Sanchez Ortiz <jorgesoibz@gmail.com>
2025-05-26 18:37:01 +0000

    proyecto cartas

--- src/app.js
+++ src/app.js
@@ -5,17 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function generarCarta() {
   //write your code here
   let valor = [ "A","2","3","4","5","6","7","8","9","J","Q","K"]
-  let suit = ["&spades;","&clubs;","&hearts;","&diams;"]
+  let suit = [{Symbol:"♠",className:"spades"},
+    {Symbol:"♣", className:"clubs"},
+    {Symbol:"♥", className:"hearts"},
+    {Symbol:"♦", className:"diams"}]
   let valorRandom = valor[Math.floor(Math.random()*valor.length)];
   let suitRandom = suit[Math.floor(Math.random()*suit.length)];
 
   document.querySelector(".numero").innerHTML = valorRandom;
-  document.querySelector(".palo-alto").innerHTML = suitRandom;
-    document.querySelector(".palo-bajo").innerHTML = suitRandom;
+  document.querySelector(".palo-alto").innerHTML = suitRandom.Symbol ;
+    document.querySelector(".palo-bajo").innerHTML = suitRandom.Symbol ;
 
-  
-  
+    document.querySelector(".palo-alto").className = "palo-alto " + suitRandom.className;
+  document.querySelector(".palo-bajo").className = "palo-bajo " + suitRandom.className;
 };
+
+document.querySelector("#btn").addEventListener("click",generarCarta);
+window.onload=generarCarta;
+setInterval(generarCarta, 3000); // intervalo de tres segundos 
